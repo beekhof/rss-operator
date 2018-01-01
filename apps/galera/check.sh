cad2a675a4089b5c9e8c786c71773242f25b482c
Andrew Beekhof <andrew@beekhof.net>
2018-01-02 10:19:21 +1100

    Add the check script

---
+++ apps/galera/check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+: ${OCF_ROOT=/usr/lib/ocf}
+: ${OCF_FUNCTIONS_DIR=${OCF_ROOT}/lib/heartbeat}
+. ${OCF_FUNCTIONS_DIR}/ocf-shellfuncs
+. ${OCF_FUNCTIONS_DIR}/mysql-common.sh
+
+set -x
+mysql_common_status err
