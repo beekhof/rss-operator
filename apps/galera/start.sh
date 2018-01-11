951b31d623ebe3c20868759d69f87203aaa85173
Andrew Beekhof <andrew@beekhof.net>
2018-01-11 22:45:02 +1100

    Check peers are actually in DNS

--- apps/galera/start.sh
+++ apps/galera/start.sh
@@ -9,6 +9,8 @@
 ocf_log info "Replicating state from $(gcomm_from_args $*)..."
 OCF_RESKEY_enable_creation=false
 
+for peer in $* ; do nslookup $peer; done
+
 mysql_common_prepare_dirs
 mysql_common_start "--wsrep-cluster-address=$(gcomm_from_args $*)"
 handle_result "replication" $?
