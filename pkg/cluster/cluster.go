b54dde63c9cfe7eed240243e7e88535f489ce865
Andrew Beekhof <andrew@beekhof.net>
2018-01-30 16:19:53 +1100

    Debug logging

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -529,6 +529,7 @@ func (c *Cluster) updateMemberStatus(members etcdutil.MemberSet, running []strin
 		}
 	}
 
+	c.logger.Infof("updateMemberStatus:  pods %v", running)
 	c.status.Replicas = len(running)
 	c.status.Members.Ready = running
 	c.status.Members.Failed = failed
