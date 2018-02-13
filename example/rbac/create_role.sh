047a5d6f35f37852e7b2fc2e600116f3b825f773
Andrew Beekhof <andrew@beekhof.net>
2018-02-13 16:34:34 +1100

    Scripts: Allow '--name value' option passing for rbac script

--- example/rbac/create_role.sh
+++ example/rbac/create_role.sh
@@ -1,59 +1,58 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 set -o errexit
-set -o nounset
 set -o pipefail
 
-ETCD_OPERATOR_ROOT=$(dirname "${BASH_SOURCE}")/../..
+OPERATOR_ROOT=$(dirname "${BASH_SOURCE}")/../..
 
 print_usage() {
-  echo "$(basename "$0") - Create Kubernetes RBAC role and role bindings for etcd-operator
+  echo "$(basename "$0") - Create Kubernetes RBAC role and role bindings for rss-operator
 Usage: $(basename "$0") [options...]
 Options:
   --role-name=STRING         Name of ClusterRole to create
-                               (default=\"etcd-operator\", environment variable: ROLE_NAME)
+                               (default=\"rss-operator\", environment variable: ROLE_NAME)
   --role-binding-name=STRING Name of ClusterRoleBinding to create
-                               (default=\"etcd-operator\", environment variable: ROLE_BINDING_NAME)
+                               (default=\"rss-operator\", environment variable: ROLE_BINDING_NAME)
   --namespace=STRING         namespace to create role and role binding in. Must already exist.
                                (default=\"default\", environment vairable: NAMESPACE)
 " >&2
 }
 
-ROLE_NAME="${ROLE_NAME:-etcd-operator}"
-ROLE_BINDING_NAME="${ROLE_BINDING_NAME:-etcd-operator}"
+ROLE_NAME="${ROLE_NAME:-rss-operator}"
+ROLE_BINDING_NAME="${ROLE_BINDING_NAME:-rss-operator}"
 NAMESPACE="${NAMESPACE:-default}"
 
-for i in "$@"
-do
-case $i in
-    --role-name=*)
-    ROLE_NAME="${i#*=}"
-    ;;
-    --role-binding-name=*)
-    ROLE_BINDING_NAME="${i#*=}"
-    ;;
-    --namespace=*)
-    NAMESPACE="${i#*=}"
-    ;;
-    -h|--help)
-      print_usage
-      exit 0
-    ;;
-    *)
-      print_usage
-      exit 1
-    ;;
-esac
+while [ 1 = 1 ] ; do
+  case "$1" in
+       --role-name)
+          ROLE_NAME="$2"; shift; shift;;
+       --role-binding-name)
+          ROLE_BINDING_NAME="$2"; shift; shift;;
+       --namespace)
+          NAMESPACE="$2"; shift; shift;;
+       -h|--help)
+          print_usage
+          exit 0
+          ;;
+       --) shift; break;;
+       "") break;;
+       *)
+          echo "Bad arg: $1"
+          print_usage
+          exit 1
+          ;;
+  esac
 done
 
 echo "Creating role with ROLE_NAME=${ROLE_NAME}, NAMESPACE=${NAMESPACE}"
 sed -e "s/<ROLE_NAME>/${ROLE_NAME}/g" \
   -e "s/<NAMESPACE>/${NAMESPACE}/g" \
-  "${ETCD_OPERATOR_ROOT}/example/rbac/cluster-role-template.yaml" | \
-  kubectl create -f -
+  "${OPERATOR_ROOT}/example/rbac/cluster-role-template.yaml" | \
+  kubectl -n ${NAMESPACE} create -f -
 
 echo "Creating role binding with ROLE_NAME=${ROLE_NAME}, ROLE_BINDING_NAME=${ROLE_BINDING_NAME}, NAMESPACE=${NAMESPACE}"
 sed -e "s/<ROLE_NAME>/${ROLE_NAME}/g" \
   -e "s/<ROLE_BINDING_NAME>/${ROLE_BINDING_NAME}/g" \
   -e "s/<NAMESPACE>/${NAMESPACE}/g" \
-  "${ETCD_OPERATOR_ROOT}/example/rbac/cluster-role-binding-template.yaml" | \
-  kubectl create -f -
+  "${OPERATOR_ROOT}/example/rbac/cluster-role-binding-template.yaml" | \
+  kubectl -n ${NAMESPACE} create -f -
